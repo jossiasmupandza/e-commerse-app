275a37fa0b48915ec863a0b57475d7b95a0ceef2
jossias <mupandzaj@gmail.com>
2021-09-23 12:43:48 +0200

    add: includes

--- skinet-api/Application/Repositories/ProductRepository.cs
+++ skinet-api/Application/Repositories/ProductRepository.cs
@@ -18,12 +18,18 @@ namespace Infrastracture.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .ToListAsync();
         }
 
         public async Task<ProductBrand> GetProductBrandByIdAsync(int id)
