ab3a7566038d4da9eedad57201738ab2a00bd363
jossias <mupandzaj@gmail.com>
2021-09-29 11:38:39 +0200

    add: specification evaluator

---
+++ skinet-api/Infrastracture/Data/SpecificationEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Application.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastracture.Data
+{
+    public class SpecificationEvaluator<TEntiy> where TEntiy : BaseEntity
+    {
+        public static IQueryable<TEntiy> GetQuery(IQueryable<TEntiy> inputQuery, ISpecification<TEntiy> spec)
+        {
+            var query = inputQuery;
+
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) =>
+                current.Include(include));
+
+            return query;
+        }
+    }
+}
