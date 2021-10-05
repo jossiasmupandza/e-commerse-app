098bf5a8f58f07961e96c7722405f5b1a64c32e5
jossias <mupandzaj@gmail.com>
2021-10-05 09:40:32 +0200

    add: creating a consistent api response for errors

---
+++ skinet-api/API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFound()
+        {
+            var thing = _context.Products.Find(34);
+
+            if (thing == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+            
+            return Ok();
+        }
+        
+        [HttpGet("serverError")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(34);
+
+            var thongToReturn = thing.ToString();
+            
+            return Ok();
+        }
+        
+        [HttpGet("badRequest")]
+        public ActionResult BadRequests()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+        
+        [HttpGet("badRequest/{id}")]
+        public ActionResult BadRequests(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ skinet-api/API/Errors/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultErrorMessage(statusCode);
+        }
+
+        private string GetDefaultErrorMessage(int statusCode)
+        {
+            return StatusCode switch
+            {
+                400 => "A bad request you have made",
+                401 => "You are not authorized",
+                404 => "Resource not found",
+                500 =>
+                    "Errors are the path to dark side. errors lead to anger. anger leads to hate. hate leads to change your work",
+                _ => "Internal error"
+            };
+        }
+    }
+}
