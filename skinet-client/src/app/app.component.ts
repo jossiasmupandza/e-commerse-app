e974f9d6a25da36f0ce8c33b41a5057fc061b1b6
jossias mupandza <mupandzaj@gmail.com>
2021-11-24 12:01:51 +0200

    client: Organize file structure

--- skinet-client/src/app/app.component.ts
+++ skinet-client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {IPagination} from "./models/pagination";
-import {IProduct} from "./models/product";
+import {IPagination} from "./shared/models/pagination";
+import {IProduct} from "./shared/models/product";
 
 @Component({
   selector: 'app-root',
