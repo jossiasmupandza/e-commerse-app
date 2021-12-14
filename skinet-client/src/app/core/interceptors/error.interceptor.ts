37e16e09613e9c1be1232b33a9898b1c895e75d0
jossias mupandza <mupandzaj@gmail.com>
2021-12-14 16:20:48 +0200

    Client: Adding toast notifications

--- skinet-client/src/app/core/interceptors/error.interceptor.ts
+++ skinet-client/src/app/core/interceptors/error.interceptor.ts
@@ -8,22 +8,32 @@ import {
 import {Observable, throwError} from 'rxjs';
 import {Router} from "@angular/router";
 import {catchError} from "rxjs/operators";
+import { ToastrService} from "ngx-toastr";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private route: Router) {}
+  constructor(private route: Router, private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError(err => {
         if(err) {
-          if(err.status === 500) {
-            this.route.navigateByUrl('/server-error');
+
+          if(err.status === 400) {
+            this.toastr.error(err.error.errorMessage, err.error.statusCode);
+          }
+
+          if(err.status === 401) {
+            this.toastr.error(err.error.errorMessage, err.error.statusCode);
           }
 
           if(err.status === 404) {
             this.route.navigateByUrl('/not-found');
+          }
+
+          if(err.status === 500) {
+            this.route.navigateByUrl('/server-error');
           }
         }
         return throwError(err);
