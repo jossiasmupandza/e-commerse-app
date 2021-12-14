ec04859698e5af36d496e944304f4ba99eb774aa
jossias mupandza <mupandzaj@gmail.com>
2021-12-14 12:39:06 +0200

    client: creating a component for testing the errors

--- skinet-client/src/app/core/core.module.ts
+++ skinet-client/src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {RouterModule} from "@angular/router";
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    TestErrorComponent
   ]
 })
 export class CoreModule { }
---
+++ skinet-client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/serverError').subscribe(res => {
+      console.log(res)
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/notfound').subscribe(res => {
+      console.log(res)
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badRequest').subscribe(res => {
+      console.log(res)
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'buggy/badRequest/one').subscribe(res => {
+      console.log(res)
+    }, error => {
+      console.log(error);
+    })
+  }
+
+}
