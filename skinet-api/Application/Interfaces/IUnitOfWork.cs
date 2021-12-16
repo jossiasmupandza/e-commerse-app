22eaa5905a778b6b83f1dd924b76b301518ef33a
jossias mupandza <mupandzaj@gmail.com>
2021-12-16 12:25:38 +0200

    API: Implementing unit of work

---
+++ skinet-api/Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+        Task<int> Complete();
+    }
+}
---
+++ skinet-api/Infrastracture/Data/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain;
+using Persistence;
+
+namespace Infrastracture.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+        private Hashtable _repositories;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> Complete()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            if(_repositories == null) _repositories = new Hashtable();
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+                var reposistoryInstance =
+                    Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
+                
+                _repositories.Add(type, reposistoryInstance);
+            }
+
+            return (IGenericRepository<TEntity>) _repositories[type];
+        }
+
+    }
+}
