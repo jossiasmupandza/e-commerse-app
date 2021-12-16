9f490033209c546691889d9feeddbd5bdad95c5b
jossias mupandza <mupandzaj@gmail.com>
2021-12-16 12:35:39 +0200

    API: Updating generic repository with unit of work methods

--- skinet-api/Application/Interfaces/IGenericRepository.cs
+++ skinet-api/Application/Interfaces/IGenericRepository.cs
@@ -11,5 +11,8 @@ namespace Application.Interfaces
         Task<T> GetEntityWithSpecification(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<int> CountAsync(ISpecification<T> spec);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
