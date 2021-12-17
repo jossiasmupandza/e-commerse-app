122441aba01b6ee70104bba9f378917aea3377f0
jossias mupandza <mupandzaj@gmail.com>
2021-12-17 10:59:02 +0200

    Client: Adding breadcrumbs

--- skinet-client/src/app/app-routing.module.ts
+++ skinet-client/src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import {NotFoundComponent} from "./core/not-found/not-found.component";
 import {ServerErrorComponent} from "./core/server-error/server-error.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)}, //layzy loading - the module will only be loaded when the link is activated
-  {path: '**', redirectTo:'', pathMatch: 'full'},
+  {path: '', component: HomeComponent, data:{breadcrumb: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent, data:{breadcrumb: 'Test Erros'}},
+  {path: 'not-found', component: NotFoundComponent, data:{breadcrumb: 'Not found'}},
+  {path: 'server-error', component: ServerErrorComponent, data:{breadcrumb: 'Server Error'}},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data:{breadcrumb: 'Shop'}}, //layzy loading - the module will only be loaded when the link is activated
+  {path: '**', redirectTo:'not-found', pathMatch: 'full'},
 ];
 
 @NgModule({
