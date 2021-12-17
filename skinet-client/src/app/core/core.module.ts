98e1b3f0033f97fba7d97507c782f25b0c0b6d77
jossias mupandza <mupandzaj@gmail.com>
2021-12-17 10:49:10 +0200

    Client: Adding section header

--- skinet-client/src/app/core/core.module.ts
+++ skinet-client/src/app/core/core.module.ts
@@ -6,11 +6,12 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import {ToastrModule} from "ngx-toastr";
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -21,9 +22,7 @@ import {ToastrModule} from "ngx-toastr";
   ],
   exports:[
     NavBarComponent,
-    TestErrorComponent,
-    NotFoundComponent,
-    ServerErrorComponent
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
