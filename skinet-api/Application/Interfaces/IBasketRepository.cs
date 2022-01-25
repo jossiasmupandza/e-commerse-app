64a1492aa81dd92684a2688914ea08902435565d
jossias mupandza <mupandzaj@gmail.com>
2022-01-25 13:21:20 +0200

    API: implementing basket interface

---
+++ skinet-api/Application/Interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Domain;
+
+namespace Application.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(string basketId);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
---
+++ skinet-api/Infrastracture/Data/BasketRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain;
+
+namespace Infrastracture.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CustomerBasket> UpdateBasketAsync(string basketId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteBasketAsync(string basketId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
