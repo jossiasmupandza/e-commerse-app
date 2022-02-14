a3adb2f2fed9367527c9f0782a08f85f8228e798
jossias mupandza <mupandzaj@gmail.com>
2022-02-14 15:16:46 +0200

    Client: Creating basket module

--- skinet-client/src/app/app-routing.module.ts
+++ skinet-client/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent, data:{breadcrumb: 'Not found'}},
   {path: 'server-error', component: ServerErrorComponent, data:{breadcrumb: 'Server Error'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data:{breadcrumb: 'Shop'}}, //layzy loading - the module will only be loaded when the link is activated
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data:{breadcrumb: 'Basket'}},
   {path: '**', redirectTo:'not-found', pathMatch: 'full'},
 ];
 
