668b48cd0e3855e405b4a09a3cb981a6b2879a2e
jossias mupandza <mupandzaj@gmail.com>
2022-03-23 15:20:43 +0200

    API: adding seed user class

---
+++ skinet-api/Persistence/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace Persistence.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async void SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Bob",
+                    Email = "bob@test.com",
+                    UserName = "bob@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Bob",
+                        LastName = "Joe",
+                        Street = "Av. Acordos de Lusaka",
+                        City = "Maputo",
+                        State = "Kamaxaquene",
+                        ZipCode = "423423"
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+    }
+}
