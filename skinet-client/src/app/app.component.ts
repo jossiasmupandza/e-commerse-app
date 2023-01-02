9f97b4320c575edf2fde4bbfd273f21bee000ac1
4052714 <mupandzaj@gmail.com>
2023-01-02 10:49:48 +0200

    Client: Persisting the login

--- skinet-client/src/app/app.component.ts
+++ skinet-client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BasketService} from "./basket/basket.service";
+import {AccountService} from "./account/account.service";
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,32 @@ import {BasketService} from "./basket/basket.service";
 export class AppComponent implements OnInit{
   title = 'skinet-client';
 
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService, private accountService: AccountService) {
   }
 
   ngOnInit(): void {
+    this.loadCurrentUser();
+    this.loadBasket();
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if(basketId) {
       this.basketService.getBasket(basketId).subscribe(()=> {
         console.log('Basket initialised');
       }, error => {
         console.log(error);
+      })
+    }
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('User loaded')
+      }, error => {
+        console.log(error)
       })
     }
   }
