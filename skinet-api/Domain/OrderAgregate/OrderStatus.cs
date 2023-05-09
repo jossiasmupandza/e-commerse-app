e27e4933603a4a1100e59419d163d0086d6cd673
jossias mupandza <mupandzaj@gmail.com>
2023-05-09 15:49:53 +0200

    API: Creating order agregate part 1

---
+++ skinet-api/Domain/OrderAgregate/OrderStatus.cs
@@ -0,0 +1,14 @@
+using System.Runtime.Serialization;
+
+namespace Domain.OrderAgregate
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value = "Pending")]
+        Pending,
+        [EnumMember(Value = "Payment Received")]
+        PaymentReceived,
+        [EnumMember(Value = "Payment Failed")]
+        PaymentFailed
+    }
+}
